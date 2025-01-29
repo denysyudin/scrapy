3b8ebc3833f45da4f7eeacc0fe349b0a3f3962e1
denysyudin <denysyudin182@gmail.com>
2025-01-29 18:02:04 +0200

    fix: add comment9

--- superbreakers/spiders/spider.py
+++ superbreakers/spiders/spider.py
@@ -35,24 +35,25 @@ class SpiderSpider(scrapy.Spider):
     def parse(self, response):
         container = response.xpath('//section')
         products = container.xpath('.//div[@class="v-product"]')
-        for product in products:
-            product_link = product.xpath('.//a[@class="v-product__img"]/@href').get()
-            yield scrapy.Request(
-                url=product_link, 
-                callback=self.parse_product
-            )
+        # for product in products:
+        product = products[0]
+        print("product", product)
+        product_link = product.xpath('.//a[@class="v-product__img"]/@href').get()
+        yield scrapy.Request(
+            url=product_link, 
+            callback=self.parse_product
+        )
         # Handle pagination
-        current_page = int(response.xpath('//input[@title="Go to page"]/@value').get())
-        total_pages = int(response.xpath('//nobr/font/b/font/b/text()').re_first(r'of (\d+)'))
-        if current_page <= total_pages:
-            print("current_page", current_page)
-            print("total_pages", total_pages)
-            next_page = f"{response.url}?searching=Y&sort=13&cat=45&show=150&page={current_page + 1}"
-            print(f"Following next page: {next_page}")
-            yield response.follow(next_page, self.parse)
+        # current_page = int(response.xpath('//input[@title="Go to page"]/@value').get())
+        # total_pages = int(response.xpath('//nobr/font/b/font/b/text()').re_first(r'of (\d+)'))
+        # if current_page <= total_pages:
+        #     print("current_page", current_page)
+        #     print("total_pages", total_pages)
+        #     next_page = f"{response.url}?searching=Y&sort=13&cat=45&show=150&page={current_page + 1}"
+        #     print(f"Following next page: {next_page}")
+        #     yield response.follow(next_page, self.parse)
 
     def parse_product(self, response):
-        print("parse_product")
         title = response.xpath('//span[@itemprop="name"]//text()').get()
         image_url = "https:" + response.xpath('//div[@class="vCSS_img_product_photo" and @id="product_photo"]/@src').get()
         # Create a JSON structure
