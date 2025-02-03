b41c4f1673708badef793687b8529755b548c2e8
denysyudin <denysyudin182@gmail.com>
2025-02-03 23:30:29 +0200

    fix comment error65

--- script.py
+++ script.py
@@ -1,13 +1,19 @@
 import os
+import dotenv
+import pandas as pd
+import numpy as np
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-import requests
+
+dotenv.load_dotenv()
 
 class RelectricCircuitBreakerScraper:
     def __init__(self):
         self.scrape_url = [
-            "https://www.relectric.com/circuit-breakers/molded-case",
+            # "https://www.relectric.com/circuit-breakers/molded-case",
             # "https://www.relectric.com/circuit-breakers/miniature",
             # "https://www.relectric.com/circuit-breakers/insulated-case",
             # "https://www.relectric.com/busway/bus-plugs",
@@ -27,6 +33,12 @@ class RelectricCircuitBreakerScraper:
             ]
 
         self.setup_driver()
+        self.create_url()
+        self.data = pd.read_csv('products.csv')
+        self.data['circuitbreakers_new'] = np.nan
+        self.data['circuitbreakers_used'] = np.nan
+        self.data['relectric_new'] = np.nan
+        self.data['relectric_used'] = np.nan
 
     def setup_driver(self):
         # Set proxy environment variables
@@ -52,6 +64,11 @@ class RelectricCircuitBreakerScraper:
         self.driver.set_page_load_timeout(30)
         self.driver.implicitly_wait(10)
 
+    def create_url(self):
+        for item in self.data['Part Number (Handle)']:
+            url = f"https://www.relectric.com/{item}"
+            self.scrape_url.append(url)
+
     @staticmethod
     def is_float(string):
         try:
@@ -59,105 +76,76 @@ class RelectricCircuitBreakerScraper:
             return True
         except ValueError:
             return False
+        
+    def insert_data(self, product_data, index):
+        data = pd.read_csv('products.csv')
+        if data['condition'][index] == 'New':
+            price = float(product_data['new_price'].replace("$", "").replace(",", ""))
+            data.at[index, 'relectricbreakers_new'] = price
 
-    def scrape_product(self, product_url):
-        try:
-            print(product_url)
-            product_url = "https://www.relectric.com/mal36800"
-            self.driver.get(product_url)
-            time.sleep(2)
-            
+        elif data['condition'][index] == 'Used':
+            price = float(product_data['re_certified_price'].replace("$", "").replace(",", ""))
+            print(f"Setting relectricbreakers_used price to {price}")
+            data.at[index, 'relectricbreakers_used'] = price
+            print(f"After setting, value is: {data.at[index, 'relectricbreakers_used']}")
+
+    def scrape_product(self):
+        for index_url, product_url in enumerate(self.scrape_url):
             try:
-                title_bar = self.driver.find_element(By.XPATH, '//div[contains(@class, "product-title-bar")]')
-                title = title_bar.find_element(By.XPATH, './/h1').text
-            except:
-                return
-
-            subtitle = title_bar.find_element(By.XPATH, '//span[@class="h4"]').text.replace('Item # ', '')
-
-            image_url = self.driver.find_element(By.XPATH, '//div[@id="main-image-wrapper"]//img').get_attribute('src')
-            
-            price_container = self.driver.find_element(By.XPATH, '//div[@id="product-buy-box"]')
-            price_list = price_container.find_elements(By.XPATH, './/p[@class="price-att"]')
-            title_list = price_container.find_elements(By.XPATH, './/span[@class="name-att"]')
-            re_certified_price = 'NA'
-            new_price = 'NA'
-            for index, name in enumerate(title_list):
-                if name.text.lower() == 're-certified':
-                    re_certified_price = price_list[index].text.strip('$').replace(',', '')
-                elif name.text.lower() == 're-certified plus':
-                    pass
-                elif name.text.lower() == 'new':
-                    new_price = price_list[index].text.strip('$').replace(',', '')
-                elif name.text.lower() == 'new surplus':
-                    new_price = price_list[index].text.strip('$').replace(',', '')
-            re_certified_price = float(re_certified_price) if self.is_float(re_certified_price) else 'NA'
-            new_price = float(new_price) if self.is_float(new_price) else 'NA'
-                
-            specification_table = self.driver.find_elements(By.XPATH, '//table//tbody//tr')
-            specifications = {}
-            for specification in specification_table:
-                specification_name = specification.find_element(By.XPATH, './/td[1]').text
-                specification_value = specification.find_element(By.XPATH, './/td[2]').text
-                specifications[specification_name] = specification_value
-
-            product_data = {
-                "data":{
-                    'title': title,
-                    'subtitle': subtitle,
-                    'image_url': image_url,
-                    're_certified_price': re_certified_price,
-                    'new_price': new_price,
-                    'specifications': specifications
-                }
-            }
-            # print(product_data)
-            response = requests.post('http://localhost:8000/api/superbreakers', json=product_data)
-            print(response.json())
-        except Exception as e:
-            print(f"Error scraping product {product_url}: {str(e)}")
-
-            try:
-                self.driver.quit()
-            except:
-                pass
-            self.setup_driver()
-            return None
-
-    def scrape_all_products(self):
-        for url in self.scrape_url:
-            try:
-                self.driver.get(url)
-                page_number = 29
+                print(product_url)
+                self.driver.get(product_url)
                 time.sleep(2)
-                while True:
-                    try:
-                        product_container = self.driver.find_element(By.XPATH, '//ol[@class="ais-Hits-list"]')
-                        products = product_container.find_elements(By.XPATH, './/li[@class="ais-Hits-item"]')
+                
+                try:
+                    title_bar = self.driver.find_element(By.XPATH, '//div[contains(@class, "product-title-bar")]')
+                    title = title_bar.find_element(By.XPATH, './/h1').text
+                except:
+                    return
+
+                subtitle = title_bar.find_element(By.XPATH, '//span[@class="h4"]').text.replace('Item # ', '')
+
+                image_url = self.driver.find_element(By.XPATH, '//div[@id="main-image-wrapper"]//img').get_attribute('src')
+                
+                price_container = self.driver.find_element(By.XPATH, '//div[@id="product-buy-box"]')
+                price_list = price_container.find_elements(By.XPATH, './/p[@class="price-att"]')
+                title_list = price_container.find_elements(By.XPATH, './/span[@class="name-att"]')
+                re_certified_price = 'NA'
+                new_price = 'NA'
+                for index, name in enumerate(title_list):
+                    if name.text.lower() == 're-certified':
+                        re_certified_price = price_list[index].text.strip('$').replace(',', '')
+                    elif name.text.lower() == 're-certified plus':
+                        pass
+                    elif name.text.lower() == 'new':
+                        new_price = price_list[index].text.strip('$').replace(',', '')
+                    elif name.text.lower() == 'new surplus':
+                        new_price = price_list[index].text.strip('$').replace(',', '')
+                re_certified_price = float(re_certified_price) if self.is_float(re_certified_price) else 'NA'
+                new_price = float(new_price) if self.is_float(new_price) else 'NA'
                     
-                        product_links = [product.find_element(By.XPATH, './/a').get_attribute('href') for product in products]
-                    
-                        for product_link in product_links:
-                            self.scrape_product(product_link)
-                        # self.driver.quit()
-                        # self.setup_driver()
-                        self.driver.get(url)
-                        time.sleep(2)
-                    
-                        for i in range(page_number):
-                            print(f'click {i}')
-                            pagination_container = self.driver.find_element(By.XPATH, '//div[@class="ais-Pagination"]')
-                            next_button = pagination_container.find_element(By.XPATH, '//li[@class="ais-Pagination-item ais-Pagination-item--nextPage"]//a')
-                            self.driver.execute_script("arguments[0].click();", next_button)
-                            time.sleep(3)
-                        page_number += 1
-                        print(f'Page {page_number}')
-                    except Exception as e:
-                        print(e)
-                        break
+                specification_table = self.driver.find_elements(By.XPATH, '//table//tbody//tr')
+                specifications = {}
+                for specification in specification_table:
+                    specification_name = specification.find_element(By.XPATH, './/td[1]').text
+                    specification_value = specification.find_element(By.XPATH, './/td[2]').text
+                    specifications[specification_name] = specification_value
+
+                product_data = {
+                    "data":{
+                        'title': title,
+                        'subtitle': subtitle,
+                        'image_url': image_url,
+                        're_certified_price': re_certified_price,
+                        'new_price': new_price,
+                        'specifications': specifications
+                    }
+                }
+                self.insert_data(product_data, index_url)
+                # # print(product_data)
+                # response = requests.post('http://localhost:8000/api/superbreakers', json=product_data)
+                # print(response.json())
             except Exception as e:
-                print(f"Error scraping {url}: {str(e)}")
-                continue
+                print(f"Error scraping product {product_url}: {str(e)}")
 
     def cleanup(self):
         try:
@@ -170,9 +158,11 @@ class RelectricCircuitBreakerScraper:
 def main():
     scraper = RelectricCircuitBreakerScraper()
     try:
-        scraper.scrape_all_products()
+        print(scraper.scrape_url)
+        # scraper.scrape_product()
     finally:
         scraper.cleanup()
+        scraper.data.to_csv('updated_products.csv', index=False)
 
 if __name__ == "__main__":
     main()
